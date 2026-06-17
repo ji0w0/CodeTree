c7e4e476b62ba3cd4bb8196cf33a37a92a518874
jiwoo kang <85946408+ji0w0@users.noreply.github.com>
2026-06-17 18:37:55 +0900

    [Passed] 함수를 이용한 윤년 판별 (2ms, 0MB)

---
+++ trail2/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+    cin >> y;
+
+    if (y % 4 == 0)
+    {
+        if (y % 100 == 0)
+        {
+            if (y % 400 == 0)
+                cout << "true" << endl;
+            else
+                cout << "false" << endl;
+        }
+        else
+            cout << "true" << endl;
+    }
+    else
+        cout << "false" << endl;
+    return 0;
+}
