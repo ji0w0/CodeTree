ae84de36964a26b152d6c2bc2acbef05753071f7
jiwoo kang <85946408+ji0w0@users.noreply.github.com>
2026-06-22 22:06:40 +0900

    [Passed] 데이터 비교 (30ms, 1MB)

---
+++ trail5/데이터 비교/data-comparison.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int N, M;
+    cin >> N;
+
+    unordered_set<int> set;
+    for (int i = 1; i <= N; ++i) {
+        int k;
+        cin >> k;
+        set.insert(k);
+    }
+
+    cin >> M;
+    for (int i = 1; i <= M; ++i) {
+        int k;
+        cin >> k;
+        auto it = set.find(k);
+        if (it != set.end()) {
+            cout << true << ' ';
+        } else {
+            cout << false << ' ';
+        }
+    }
+    
+    return 0;
+}
